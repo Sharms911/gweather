cf27cfc1074bb04c2d2bacc6873b513b15dbc531
Sharms911 <132896436+Sharms911@users.noreply.github.com>
2024-03-08 15:05:07 +1030

    added a home page

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("hello world")
+from flask import Flask, render_template, request
+import requests
+
+app = flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
